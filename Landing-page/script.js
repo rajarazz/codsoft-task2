50f182511027524df84f93b68dbede4e95328647
Raja Raj <125489767+rajarazz@users.noreply.github.com>
2024-01-20 20:49:42 +0530

    Add files via upload

---
+++ Landing-page/script.js
@@ -0,0 +1,12 @@
+
+let container = document.querySelector(".container");
+
+redbtn.addEventListener("click", function () {
+    container.style.backgroundImage = "url('BMW_bike_img/BMW1.png')";
+});
+bluebtn.addEventListener("click", function () {
+    container.style.backgroundImage = "url('BMW_bike_img/BMW2.png')";
+});
+blackbtn.addEventListener("click", function () {    
+    container.style.backgroundImage = "url('BMW_bike_img/BMW3.png')";
+});
